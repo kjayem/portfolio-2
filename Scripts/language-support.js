ec1d7a29118283b06022b1b4e5176f2268183832
kjayem <flwfeeld@gmail.com>
2021-07-24 21:19:35 +0900

    added nav language translate support

--- Scripts/language-support.js
+++ Scripts/language-support.js
@@ -1,23 +1,46 @@
-
+//supported translations
 var language = {
     "us":{
         "nav about": "ABOUT",
         "nav collection": "COLLECTION",
         "nav shop": "SHOP",
-        "nav lang": "EN",
         "nav login": "LOGIN",
     },
     "kr":{
         "nav about": "소개",
         "nav collection": "컬렉션",
         "nav shop": "제품",
-        "nav lang": "KR",
         "nav login": "로그인",
     }
 }
 
-document.getElementById('change-language').addEventListener('click', getLang);
 
-function getLang() {
-    console.log(language.us["nav about"]);
+//change language button
+var langButton = document.getElementById('change-language');
+langButton.addEventListener('click', changeLang);
+
+function changeLang() {
+    var textToTranslate = document.querySelectorAll(".nav-lang")
+    var koreanCategory = [];
+    var koreanKey = Object.keys(language.kr);
+    var englishCategory = [];
+    var englishKey = Object.keys(language.us);
+
+    if (langButton.classList.contains('current-lang-english')) {
+        for (var i = 0; i < koreanKey.length; i++) {
+            koreanCategory.push(language.kr[koreanKey[i]]);
+            textToTranslate[i].innerHTML = koreanCategory[i];
+            console.log(koreanCategory);
+        }
+        langButton.className = 'current-lang-korean';
+        langButton.innerHTML = 'KR &nbsp;<span class="flag-icon flag-icon-kr"></span>';
+    } else if (langButton.classList.contains('current-lang-korean')) {
+        for (var i = 0; i < englishKey.length; i++) {
+            englishCategory.push(language.us[englishKey[i]]);
+            textToTranslate[i].innerHTML = englishCategory[i];
+            console.log(englishCategory);
+        }
+        langButton.className = 'current-lang-english';
+        langButton.innerHTML = 'EN &nbsp;<span class="flag-icon flag-icon-us"></span>';
+    }
 }
