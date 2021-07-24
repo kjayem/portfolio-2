92b3d02fa5a11f657677dff0129fd3b175ae419d
kjayem <flwfeeld@gmail.com>
2021-07-25 01:06:29 +0900

    cleaned up code

--- Scripts/language-support.js
+++ Scripts/language-support.js
@@ -27,23 +27,23 @@ langButton.addEventListener('click', changeLang);
 function changeLang() {
     var textToTranslate = document.querySelectorAll(".translatable")
     var koreanCategory = [];
-    var koreanKey = Object.keys(language.kr);
+    var key = Object.keys(language.us);
     var englishCategory = [];
-    var englishKey = Object.keys(language.us);
+    // var englishKey = Object.keys(language.us);
 
     if (langButton.classList.contains('current-lang-english')) {
-        for (var i = 0; i < koreanKey.length; i++) {
-            koreanCategory.push(language.kr[koreanKey[i]]);
-            textToTranslate[i].innerHTML = koreanCategory[i];
+        for (var i = 0; i < key.length; i++) {
+            koreanCategory.push(language.kr[key[i]]);
             console.log(koreanCategory);
+            textToTranslate[i].innerHTML = koreanCategory[i];
         }
         langButton.className = 'current-lang-korean';
         langButton.innerHTML = 'KR &nbsp;<span class="flag-icon flag-icon-kr"></span>';
     } else if (langButton.classList.contains('current-lang-korean')) {
-        for (var i = 0; i < englishKey.length; i++) {
-            englishCategory.push(language.us[englishKey[i]]);
-            textToTranslate[i].innerHTML = englishCategory[i];
+        for (var i = 0; i < key.length; i++) {
+            englishCategory.push(language.us[key[i]]);
             console.log(englishCategory);
+            textToTranslate[i].innerHTML = englishCategory[i];
         }
         langButton.className = 'current-lang-english';
         langButton.innerHTML = 'EN &nbsp;<span class="flag-icon flag-icon-us"></span>';
